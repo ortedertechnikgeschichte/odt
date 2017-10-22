24dcb5e7c750b873af4e83d4d42a3a3656c03a2a
Lorenz Burger <lorenzburger@arcor.de>
2017-10-23 00:03:10 +0200

    Updates

--- src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  justify-content: space-around;
-  background: linear-gradient(top, white, grey);
-`
